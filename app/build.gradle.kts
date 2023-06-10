3ebe65dc55bf83c3603691ff859f3b391569590a
Orhan Rosenfeld <codeasus.orhan@outlook.com>
2023-06-10 21:53:05 +0400

    Add okhttp dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,10 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.10.0"))
+    implementation("com.squareup.okhttp3:okhttp")
+    implementation("com.squareup.okhttp3:logging-interceptor")
 }
 
 application {
